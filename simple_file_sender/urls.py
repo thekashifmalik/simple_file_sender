bfd3643cb44b5a294fc82d280ba764f1a82fdd40
kalail <kashif610@gmail.com>
2012-10-03 23:34:42 -0500

    Add upload view.

--- simple_file_sender/urls.py
+++ simple_file_sender/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^simple_file_sender/', include('simple_file_sender.foo.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^upload/$', 'simple_file_sender.views.upload'),
     url(r'^$', 'simple_file_sender.views.index'),
 )
--- simple_file_sender/views.py
+++ simple_file_sender/views.py
@@ -1,11 +1,15 @@
 # Create your views here.
 from django.shortcuts import render_to_response, get_object_or_404
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
 from django.contrib.auth import logout
 from django.views.decorators.cache import cache_page
-
+import pdb
 
 def index(request):
 	return render_to_response('index.html',context_instance=RequestContext(request))
+
+def upload(request):
+	
+	return HttpResponse('OK')
